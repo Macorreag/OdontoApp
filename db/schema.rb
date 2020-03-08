f48409afbe8a2951b87d494ea2f6626546dbe46b
Miller <macorreag@unal.edu.co>
2020-03-08 13:48:16 -0500

    migration added

--- db/schema.rb
+++ db/schema.rb
@@ -10,28 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_160141) do
-
-  create_table "appointments", force: :cascade do |t|
-    t.date "date"
-    t.time "time"
-    t.time "duration"
-    t.string "site"
-    t.integer "id_patient"
-    t.string "description"
-    t.integer "id_doctor"
-    t.boolean "attendance"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.integer "visits_count"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_03_08_174853) do
 
   create_table "citations", force: :cascade do |t|
     t.date "date"
@@ -44,6 +23,20 @@ ActiveRecord::Schema.define(version: 2020_03_08_160141) do
     t.boolean "attendance"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.string "rapermission_level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
